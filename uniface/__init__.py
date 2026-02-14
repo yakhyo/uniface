25663b7ce364f6cb26b13297574e3c332f9521ad
yakhyo <yakhyo9696@gmail.com>
2026-02-14 23:48:59 +0900

    release: Update release version to v3.0.0

--- uniface/__init__.py
+++ uniface/__init__.py
@@ -29,7 +29,7 @@ from __future__ import annotations
 
 __license__ = 'MIT'
 __author__ = 'Yakhyokhuja Valikhujaev'
-__version__ = '2.3.0'
+__version__ = '3.0.0'
 
 from uniface.face_utils import compute_similarity, face_alignment
 from uniface.log import Logger, enable_logging
