aa282bbb66d04926d9dddaae8d7f3769d91a454c
Bert <maxcooncat75@gmail.com>
2026-07-12 09:35:34 +0300

    Added RESNET50 for Retinaface

--- uniface/constants.py
+++ uniface/constants.py
@@ -80,6 +80,7 @@ class RetinaFaceWeights(str, Enum):
     MNET_V2  =  "retinaface_mnet_v2"
     RESNET18 =  "retinaface_r18"
     RESNET34 =  "retinaface_r34"
+    RESNET50 =  "retinaface_r50"
 
 
 class SCRFDWeights(str, Enum):
@@ -280,6 +281,10 @@ MODEL_REGISTRY: dict[Enum, ModelInfo] = {
     RetinaFaceWeights.RESNET34: ModelInfo(
         url='https://github.com/yakhyo/uniface/releases/download/weights/retinaface_r34.onnx',
         sha256='bd0263dc2a465d32859555cb1741f2d98991eb0053696e8ee33fec583d30e630'
+    ),
+    RetinaFaceWeights.RESNET50: ModelInfo(
+        url='https://huggingface.co/TheEeeeLin/HivisionIDPhotos_matting/resolve/main/retinaface-resnet50.onnx',
+        sha256='40f825cf7dd0a88b26fb61db9a3aaedc2cad35162091113f4017b3c26a4f792d'
     ),
 
     # MobileFace
